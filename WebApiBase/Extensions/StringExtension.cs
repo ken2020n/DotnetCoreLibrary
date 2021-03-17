fbbf4911440ad44fe728fb439e130e675c2c37ce
user <user@users-MacBook-Pro.local>
2021-03-18 03:32:10 +0800

    refactoring

--- WebApiBase/Extensions/StringExtension.cs
+++ WebApiBase/Extensions/StringExtension.cs
@@ -10,10 +10,7 @@ namespace WebApiBase.Extensions
             var bytes = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(value));
 
             var stringBuilder = new StringBuilder();
-            foreach (var item in bytes)
-            {
-                stringBuilder.Append(item.ToString("x2"));
-            }
+            foreach (var item in bytes) stringBuilder.Append(item.ToString("x2"));
 
             return stringBuilder.ToString();
         }
